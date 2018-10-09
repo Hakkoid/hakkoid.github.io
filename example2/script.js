5d7bca96de23f433bda055e8c2d7ef77e6226039
Hakkoid <brichencov@yandex.ru>
2018-10-09 12:06:26 +0300

    update

---
+++ example2/script.js
@@ -0,0 +1,75 @@
+'use strict'
+
+var widgets = {};
+
+widgets.state = {
+	center: "center"
+};
+
+var left = document.getElementById('image-left');
+var center = document.getElementById('image-center');
+var right = document.getElementById('image-right');
+
+document.getElementById("widgets__left-arrow").onclick = function(){
+	
+	 switch(widgets.state.center){
+	 	case 'center':
+			scrollRight(center, left, right);
+			widgets.state.center = 'right';
+	 		break;
+	 	case 'left':
+	 		scrollRight(right, center, left);
+			widgets.state.center = 'center';
+	 		break;
+	 	case 'right':
+	 		scrollRight(left, right, center);
+			widgets.state.center = 'left';
+	 		break;
+	}
+}
+
+
+document.getElementById("widgets__right-arrow").onclick = function(){
+	 switch(widgets.state.center){
+	 	case 'center':
+			scrollLeft(center, left, right);
+			widgets.state.center = 'left';
+	 		break;
+	 	case 'left':
+	 		scrollLeft(right, center, left);
+			widgets.state.center = 'right';
+	 		break;
+	 	case 'right':
+	 		scrollLeft(left, right, center);
+			widgets.state.center = 'center';
+	 		break;
+	 }
+}
+
+function scrollLeft(center, left, right){
+	clearClasses(center);
+	center.classList.add('widgets__images_center-to-left');
+	clearClasses(left);
+	left.classList.add('widgets__images_left-to-right');
+	clearClasses(right);
+	right.classList.add('widgets__images_right-to-center');
+};
+
+function scrollRight(center, left, right){
+	clearClasses(center);
+	center.classList.add('widgets__images_center-to-right');
+	clearClasses(left);
+	left.classList.add('widgets__images_left-to-center');
+	clearClasses(right);
+	right.classList.add('widgets__images_right-to-left');
+} 
+
+
+function clearClasses(elem){
+	elem.classList.remove('widgets__images_right-to-center');
+	elem.classList.remove('widgets__images_left-to-center');
+	elem.classList.remove('widgets__images_right-to-left');
+	elem.classList.remove('widgets__images_left-to-right');
+	elem.classList.remove('widgets__images_center-to-right');
+	elem.classList.remove('widgets__images_center-to-left');
+}
