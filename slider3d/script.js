f474d350627f54e397adf437982b0bb549703a3d
Hakkoid <brichencov@yandex.ru>
2018-11-27 13:06:45 +0300

    update

--- slider3d/script.js
+++ slider3d/script.js
@@ -4,15 +4,18 @@ function initSlider(){
 		number = 0,
 		wrapper = $("#slider__wrapper"),
 		slides = $(".slider__slide"),
-		length = wrapper.children().length;
-		console.log(length)
+		view = $("#slider__view"),
+		length = wrapper.children().length,
+		width = wrapper.width();
+
+	view.css("perspective", wrapper.width * length * 2);
 
 	$.each(slides, function(index, value){
-		$(this).css("transform", "rotateY(" + index * 360 / length + "deg)" + " translateZ(" + wrapper.width() * 1.3 + "px)")
+		$(this).css("transform", "rotateY(" + index * 360 / length + "deg)" + 
+		" translateZ(" + (width/(2 * getTanDeg(180/length)) + 50) + "px)")
 	})
 
 	right.on('click', function(){
-
 		number++;
 		wrapper.css("transform", "rotateY(" + - number * 360 / length + "deg)");
 	})
@@ -22,6 +25,11 @@ function initSlider(){
 		number--;
 		wrapper.css("transform", "rotateY(" + - number * 360 / length + "deg");
 	})
+}
+
+function getTanDeg(deg) {
+  var rad = deg * Math.PI/180;
+  return Math.tan(rad);
 }
 
 initSlider()
