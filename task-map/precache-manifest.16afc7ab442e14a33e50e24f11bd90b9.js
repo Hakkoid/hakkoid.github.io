080efe5b0a8cd4a5af881b0f1c93635a005ff1a6
Hakkoid <brichencov@yandex.ru>
2019-01-12 17:21:23 +0300

    update

---
+++ task-map/precache-manifest.16afc7ab442e14a33e50e24f11bd90b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d1f1106bfb88a8ccaee1",
+    "url": "/static/js/main.d1f1106b.chunk.js"
+  },
+  {
+    "revision": "280d989e8e029bd6394a",
+    "url": "/static/js/1.280d989e.chunk.js"
+  },
+  {
+    "revision": "d1f1106bfb88a8ccaee1",
+    "url": "/static/css/main.82d2e0f5.chunk.css"
+  },
+  {
+    "revision": "846b359099fba9019c2bc9119b65df81",
+    "url": "/index.html"
+  }
+];
