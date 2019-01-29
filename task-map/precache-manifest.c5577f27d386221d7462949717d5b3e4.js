ba2b1147a0af2c32e04de9d00b37b311eebc5105
Hakkoid <brichencov@yandex.ru>
2019-01-29 11:12:11 +0300

    Update task-map

---
+++ task-map/precache-manifest.c5577f27d386221d7462949717d5b3e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ddc4f876c6aa912f18f5",
+    "url": "/static/js/main.ddc4f876.chunk.js"
+  },
+  {
+    "revision": "b7b0b4617a4292dfdb15",
+    "url": "/static/js/1.b7b0b461.chunk.js"
+  },
+  {
+    "revision": "ddc4f876c6aa912f18f5",
+    "url": "/static/css/main.82d2e0f5.chunk.css"
+  },
+  {
+    "revision": "b9fa193605d1d5bffbdafa1fd3efdb91",
+    "url": "/index.html"
+  }
+];
--- task-map/service-worker.js
+++ task-map/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16afc7ab442e14a33e50e24f11bd90b9.js"
+  "/precache-manifest.c5577f27d386221d7462949717d5b3e4.js"
 );
 
 workbox.clientsClaim();
